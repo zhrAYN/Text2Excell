0893e9c9548c84071e16783637273bfa71ffd8cb
zhrAYN <35058175+zhrAYN@users.noreply.github.com>
2018-01-03 10:17:42 +0300

    Add files via upload

---
+++ performansodeviveriaktarma/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace CsharpExcelVeriAktarma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnAktar = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnAc = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(463, 345);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged_1);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Ürün ID";
+            this.columnHeader1.Width = 107;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Ürün Adı";
+            this.columnHeader2.Width = 123;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Adet";
+            this.columnHeader3.Width = 88;
+            // 
+            // btnAktar
+            // 
+            this.btnAktar.BackColor = System.Drawing.Color.Pink;
+            this.btnAktar.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Underline | System.Drawing.FontStyle.Strikeout))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAktar.Location = new System.Drawing.Point(482, 285);
+            this.btnAktar.Name = "btnAktar";
+            this.btnAktar.Size = new System.Drawing.Size(181, 41);
+            this.btnAktar.TabIndex = 1;
+            this.btnAktar.Text = "Veri aktarımı yap";
+            this.btnAktar.UseVisualStyleBackColor = false;
+            this.btnAktar.Click += new System.EventHandler(this.btnAktar_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnAc
+            // 
+            this.btnAc.BackColor = System.Drawing.Color.LightPink;
+            this.btnAc.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 11.25F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline) 
+                | System.Drawing.FontStyle.Strikeout))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAc.ForeColor = System.Drawing.Color.Black;
+            this.btnAc.Location = new System.Drawing.Point(481, 233);
+            this.btnAc.Name = "btnAc";
+            this.btnAc.Size = new System.Drawing.Size(182, 46);
+            this.btnAc.TabIndex = 2;
+            this.btnAc.Text = "Excel seç";
+            this.btnAc.UseVisualStyleBackColor = false;
+            this.btnAc.Click += new System.EventHandler(this.btnAc_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightBlue;
+            this.button1.Font = new System.Drawing.Font("Algerian", 11.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(6, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightBlue;
+            this.textBox1.Location = new System.Drawing.Point(150, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.LightBlue;
+            this.textBox2.Location = new System.Drawing.Point(150, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.LightBlue;
+            this.textBox3.Location = new System.Drawing.Point(150, 76);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(482, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(289, 194);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ürün ekleme";
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.LightBlue;
+            this.textBox4.Location = new System.Drawing.Point(150, 102);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.TabIndex = 14;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Pink;
+            this.button3.Location = new System.Drawing.Point(69, 102);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Başlık ekle";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightBlue;
+            this.button2.Font = new System.Drawing.Font("Algerian", 11.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(87, 152);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 36);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Sil\r\n";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Pink;
+            this.label3.Location = new System.Drawing.Point(84, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Adet:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Pink;
+            this.label2.Location = new System.Drawing.Point(84, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Ürün adı:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Pink;
+            this.label1.Location = new System.Drawing.Point(84, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ürün ID:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(808, 359);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnAc);
+            this.Controls.Add(this.btnAktar);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Excel-C# Veri aktarımı";
+            this.TransparencyKey = System.Drawing.Color.PaleVioletRed;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button btnAktar;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnAc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+        public System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ performansodeviveriaktarma/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel= Microsoft.Office.Interop.Excel;
+
+namespace CsharpExcelVeriAktarma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listviewDoldur()
+        {
+            try
+            {
+                // 20 tane ürünü dinamik olarak eklemsi için kulandık. artırıp azaltabilirsiniz.
+                int urunSayisi = 20;
+                // For dönüsü ile ürün sayısı kadar ekleme işlemi yapıyoruz
+                for (int i = 0; i < urunSayisi; i++)
+                {
+                    // eklenecek ürünler için örnek bir dizi tanımlıyoruz
+                    string[] urunler = {
+                                       "UrunID"+i.ToString(),
+                                       "Urun Adı " + i.ToString(),
+                                       "Adet :" + i.ToString()
+                                        };
+                    ListViewItem urun = new ListViewItem(urunler);
+                    listView1.Items.Add(urun);// listview1 adında olan listview nesnemize urun itemini yüklüyoruz
+                } // for bitis
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ürün ekleme işlemi sırasında bir hata meydana geldi.\n Hata içeriği:" + ex.ToString());
+            }
+        }
+
+        /// <summary>
+        ///  ListView verilerini dinamik olarak excel dosyasına aktarır
+        /// </summary>
+        /// <param name="lw">Aktarım Yapılacak ListView nesnesinin IDsi</param>
+        public void excelAktar(ListView lw, ProgressBar pb = null)
+        {
+            try
+            {
+               
+                Excel.Application Excelapp = new Excel.Application();
+                Excel.Workbook wb = Excelapp.Workbooks.Add(Excel.XlSheetType.xlWorksheet);
+                Excel.Worksheet ws = (Excel.Worksheet)Excelapp.ActiveSheet; // çalışma alanı aktif çalışma alanı
+
+                Excelapp.Visible = true; // görünürlük aktif
+                #region manuelBaslikalani
+                // alanları manuel olarak yazıyoruz
+                /*ws.Cells[1, 1] = "Stok Kodu";6
+                ws.Cells[1, 2] = "Barkod";
+                ws.Cells[1, 3] = "Stok Adı";
+                ws.Cells[1, 4] = "Stok Miktar";
+                ws.Cells[1, 4] = "Stok Grubu";
+                ws.Cells[1, 5] = "Stok Markası";
+                */
+                #endregion
+                // Eğer Progress bar nesnesi null değil ise sıfırlama ve ayarlama işlemini gerçekleştir
+                if (pb != null)
+                {
+                    pb.Maximum = Convert.ToInt32(lw.Items.Count.ToString());
+                    pb.Value = 0;
+                }
+
+                // Şimdi ise dinamik olarak colon bilgilerini alıyoruz ekliyoruz
+                for (int i = 0; i < lw.Columns.Count; i++)
+                {
+                    // alanları manuel olarak yazıyoruz
+                    ws.Cells[1, i + 1] = lw.Columns[i].Text.ToString();
+                }
+                // Şimdi de lw içerisindeki verileri dinamik olarak aktarıyoruz
+                int _i = 2; // 2. satırdan itibaren içerikleri doldurmaya başla
+                int j = 1;
+                foreach (ListViewItem item in lw.Items)
+                {
+                    ws.Cells[_i, j] = item.Text.ToString();
+                    foreach (ListViewItem.ListViewSubItem subitem in item.SubItems)
+                    {
+                        ws.Cells[_i, j] = subitem.Text.ToString();
+                        j++;
+                    }
+                    j = 1;
+                    _i++;
+                    // Eğer Progress bar nesnesi null değil ise artırma işlemini yap
+                    if (pb != null)
+                    {
+                        pb.Value = _i - 2;
+                    }
+
+                }
+                Excelapp.Columns.AutoFit(); // column sütunları yazı boyutuna göre ayarlıyor
+                Excelapp.AlertBeforeOverwriting = false; // aktarama işlemi sırasında alabileceğimiz hatalara karşı önlem olarak hata bastırma işlemi yapılıyor
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listviewDoldur();
+        }
+
+        private void btnAktar_Click(object sender, EventArgs e)
+        {
+            excelAktar(listView1);
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAc_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Belge Seçiniz...";
+            openFileDialog1.Filter = "Excel 2003 dosyaları (*.xls)|*.xls|Excel 2010-2016 dosyaları (*.xlsx)|*.xlsx";
+            openFileDialog1.FilterIndex = 1;
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel) 
+            MessageBox.Show("Dosya Seçilemedi");
+            else 
+            {
+                Excel.Application Excelapp = new Excel.Application();
+                Excelapp.Workbooks.Open(openFileDialog1.FileName);
+                Excelapp.Visible = true;
+            }
+                        
+        }
+        //Excele ekranına ve dosyamıza ekleme yapmak için kod satırlarımız...                                     
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string  UrunID    = textBox1.Text;
+            string  UrunAdı   = textBox2.Text;
+            string  Adet      = textBox3.Text;
+            string Başlıkekle = textBox4.Text;
+
+            string[] row = { "UrunID" +UrunID, "Urun Adı " +UrunAdı, "Adet:" +Adet,  };
+            var satir = new ListViewItem(row);
+            listView1.Items.Add(satir);
+
+        }
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            string aktar = textBox4.Text;
+            listView1.Columns.Add(aktar , 100);
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
